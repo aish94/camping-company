e534a0629611a0c4797228734d19adc2ff8ccc8a
kanishktanwar <kanishk.tanwar7@gmail.com>
2019-11-23 21:43:31 +0530

    added django setting module in worker.py to fix error app did not loaded yet

--- worker.py
+++ worker.py
@@ -10,6 +10,7 @@ redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
 conn = redis.from_url(redis_url)
 
 if __name__ == '__main__':
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "camping.settings")
     with Connection(conn):
         worker = Worker(map(Queue, listen))
         worker.work()
