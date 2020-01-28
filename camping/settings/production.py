da4ed00ddc1621b91e210f7679badc0b255319c5
kanishktanwar <kanishk.tanwar7@gmail.com>
2020-01-28 12:35:16 +0530

    html compression disabled

--- camping/settings/production.py
+++ camping/settings/production.py
@@ -74,9 +74,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.gzip.GZipMiddleware',
-    'htmlmin.middleware.HtmlMinifyMiddleware',
-    'htmlmin.middleware.MarkRequestMiddleware',
+    #'django.middleware.gzip.GZipMiddleware',
+    #'htmlmin.middleware.HtmlMinifyMiddleware',
+    #'htmlmin.middleware.MarkRequestMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     #'django.middleware.cache.UpdateCacheMiddleware',
