4db492fb2bcf8aa30186f81b5f18de81227c0c41
kanishktanwar <kanishk.tanwar7@gmail.com>
2020-02-02 14:37:37 +0530

    message tupo

--- customer/utils.py
+++ customer/utils.py
@@ -11,5 +11,5 @@ def render_to_pdf(template_src, context_dict={}):
     result = BytesIO()
     pdf = pisa.pisaDocument(BytesIO(html.encode()), result)
     if not pdf.err:
-        return HttpResponse(result.getvalue(), content_type='application/pdf')
+        return HttpResponse(pdf, content_type='application/pdf')
     return None
