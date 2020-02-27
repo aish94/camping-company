fd57f76c2c8257faa573bae0d07e685ade40fae2
kanishktanwar <kanishk.tanwar7@gmail.com>
2020-02-27 13:11:42 +0530

    redirect bug fix in review views

--- reviews/views.py
+++ reviews/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from reviews.models import DestinationReview, BlogReview
 from destination.models import Destination
-from django.contrib.auth.decorators import login_required
 from blog.models import Blog
 from django.http import JsonResponse
 from datetime import date, timedelta
@@ -11,7 +10,6 @@ from django.contrib import messages
 # Create your views here.
 
 
-@login_required
 def destination_add_review(request):
     if request.is_ajax():
         if request.user.is_authenticated:
@@ -23,10 +21,9 @@ def destination_add_review(request):
         else:
             return JsonResponse({"not saved": "failure"})
     messages.warning(request, "Oh no You are lost mate!!!")
-    return render("app:home")
+    return redirect("app:home")
 
 
-@login_required
 def blog_add_review(request):
     if request.is_ajax():
         if request.user.is_authenticated:
@@ -38,4 +35,4 @@ def blog_add_review(request):
         else:
             return JsonResponse({"not saved": "failure"})
     messages.warning(request, "Oh no You are lost mate!!!")
-    return render("app:home")
+    return redirect("app:home")
