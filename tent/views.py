ece14a1754a10eb6de8257964cb9347dcc83ee88
kanishktanwar <kanishk.tanwar7@gmail.com>
2021-09-09 17:30:14 +0530

    name of link Experience to Experiences been change

--- tent/views.py
+++ tent/views.py
@@ -82,7 +82,7 @@ def cart(request):
                              "name": name,
                              "razor_id": razor_id
                              })
-    if price not in [49000, 55000]:
+    if price not in [65000, 55000]:
         messages.warning(request, "NO HACKY HACKY")
         return redirect("tent_check:all")
     return render(request, "tent/cart.html", {"price": price})
