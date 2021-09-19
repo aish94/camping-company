ad592a5919aa907ec24d06ba3a125773fadad61a
kanishktanwar <kanishk.tanwar7@gmail.com>
2021-09-19 17:28:25 +0530

    error handling if user put a wrong url in experience path

--- destination/views.py
+++ destination/views.py
@@ -542,9 +542,13 @@ def experiences(request):
 
 
 def experience_detail(request, slug):
-    experience = Experiences.objects.get(slug=slug)
-    if experience.pk in [2, 3, 4]:
-        redirect("app:home")
+    try:
+        experience = Experiences.objects.get(slug=slug)
+        if experience.pk in [2, 3, 4]:
+            return redirect("app:home")
+    except:
+        messages.warning("site does not exist")
+        return redirect("app:home")
 
     if request.is_ajax():
         if request.user.is_authenticated:
