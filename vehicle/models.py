75d71001bc76dd7ba3c222b4ab646d426c55b60a
kanishktanwar <kanishk.tanwar7@gmail.com>
2021-09-23 20:55:49 +0530

    ui change main vehicle booking page

--- vehicle/models.py
+++ vehicle/models.py
@@ -36,6 +36,7 @@ class Definition(models.Model):
     car_image = models.ImageField(upload_to="cars", blank=True, null=True)
     price = models.IntegerField()
     drive_train = models.CharField(max_length=64, blank=True, null=True)
+    description = models.TextField(blank=True, null=True)
     available = models.BooleanField(default=True)
 
     def __str__(self):
